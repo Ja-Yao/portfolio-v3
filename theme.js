254da200f50483d6f2949f2e71c0a05a66aaa261
Ja-Yao <jtyaos@gmail.com>
2022-09-21 21:11:26 -0400

    Setup styling

---
+++ theme.js
@@ -0,0 +1,42 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#c0e6ba",
+      onPrimary: "#fbfbfd",
+      primaryContainer: "#c0e6ba",
+      onPrimaryContainer: "#002105",
+
+      background: "#fbfbfd",
+      onBackground: "#1b1c1a",
+      surface: "#fbfbfd",
+      onSurface: "#1b1c1a",
+      surfaceVariant: "#e1e3dc",
+      onSurfaceVariant: "#454843",
+      outline: "#757872",
+    },
+    secondary: {
+      main: "#52634f",
+      onSecondary: "#fbfbfd",
+      secondaryContainer: "#d5e8cf",
+      onSecondaryContainer: "#101f10",
+
+      background: "#fbfbfd",
+      onBackground: "#1b1c1a",
+      surface: "#fbfbfd",
+      onSurface: "#1b1c1a",
+      surfaceVariant: "#e1e3dc",
+      onSurfaceVariant: "#454843",
+      outline: "#757872",
+    },
+  },
+  typography: {
+
+  },
+  components: {
+
+  }
+});
+
+export default theme;
