1a1d73e9e2da8e392d8ded6925001ebcd1cf5a6d
Ja-Yao <jtyaos@gmail.com>
2022-09-26 23:46:06 -0400

    Replace svg with MUI Icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,16 @@
 import { FC, useContext } from "react";
+import Link from "next/link";
 import { Box, Button, createSvgIcon, Grid, IconButton, Stack, Tooltip } from "@mui/material";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 import { useTheme } from '@mui/material/styles';
 import { ColorModeContext } from "../pages/_app";
-import styles from "../styles/Icons.module.css";
+
 
 type Props = {}
 
 const Navbar: FC<Props> = () => {
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
-
-  const ToggleIcon = createSvgIcon(
-    <g>
-      <g id="circle" className={styles["toggle-circle"]}>
-        <path fillOpacity="1" d=" M0,-4 C-2.2100000381469727,-4 -4,-2.2100000381469727 -4,0 C-4,2.2100000381469727 -2.2100000381469727,4 0,4 C2.2100000381469727,4 4,2.2100000381469727 4,0 C4,-2.2100000381469727 2.2100000381469727,-4 0,-4z">
-        </path>
-      </g>
-      <g id="sun" opacity="1" className={styles["toggle-sun"]}>
-        <path fillOpacity="1" d=" M0,6 C-3.309999942779541,6 -6,3.309999942779541 -6,0 C-6,-3.309999942779541 -3.309999942779541,-6 0,-6 C3.309999942779541,-6 6,-3.309999942779541 6,0 C6,3.309999942779541 3.309999942779541,6 0,6z M8,-3.309999942779541 C8,-3.309999942779541 8,-8 8,-8 C8,-8 3.309999942779541,-8 3.309999942779541,-8 C3.309999942779541,-8 0,-11.3100004196167 0,-11.3100004196167 C0,-11.3100004196167 -3.309999942779541,-8 -3.309999942779541,-8 C-3.309999942779541,-8 -8,-8 -8,-8 C-8,-8 -8,-3.309999942779541 -8,-3.309999942779541 C-8,-3.309999942779541 -11.3100004196167,0 -11.3100004196167,0 C-11.3100004196167,0 -8,3.309999942779541 -8,3.309999942779541 C-8,3.309999942779541 -8,8 -8,8 C-8,8 -3.309999942779541,8 -3.309999942779541,8 C-3.309999942779541,8 0,11.3100004196167 0,11.3100004196167 C0,11.3100004196167 3.309999942779541,8 3.309999942779541,8 C3.309999942779541,8 8,8 8,8 C8,8 8,3.309999942779541 8,3.309999942779541 C8,3.309999942779541 11.3100004196167,0 11.3100004196167,0 C11.3100004196167,0 8,-3.309999942779541 8,-3.309999942779541z">
-        </path>
-      </g>
-    </g>,
-    'Theme'
-  );
 
   return (
     <Grid container justifyContent='center' alignItems='center' sx={{ marginTop: 1, marginBottom: 1 }}>
@@ -34,33 +21,38 @@ const Navbar: FC<Props> = () => {
           <Stack direction="row" spacing={3} alignItems="center">
             <Tooltip title={theme.palette.mode === "dark" ? "Switch to the light theme" : "Switch to the dark theme"} placement="bottom" sx={{ fontSize: "16pt" }}>
               <IconButton aria-label="toggle theme button" sx={{ width: 'auto', height: 'auto' }} onClick={colorMode.toggleColorMode} color="inherit">
-                <ToggleIcon
-                  className={theme.palette.mode === "dark" ? "" : styles["light-theme"]}
-                  sx={{
-                    viewBox: "0 0 24 24",
-                    width: 24,
-                    height: 24,
-                    preserveAspectRatio: "xMidYMid meet",
-                    color: theme.palette.mode === "dark" ? theme.palette.grey[400] : theme.palette.grey[600],
-                    ":hover": {
-                      color: theme.palette.primary.onSurface,
-                    },
-                    transform: "translate3d(0px, 0px, 0px)",
-                  }}
-                />
+                {
+                  theme.palette.mode === "dark"
+                    ? <Brightness7 sx={{
+                      color: theme.palette.mode === "dark" ? theme.palette.grey[400] : theme.palette.grey[600],
+                      ":hover": { color: theme.palette.primary.onSurface },
+                    }}
+                    />
+                    : <Brightness4 sx={{
+                      color: theme.palette.mode === "light" ? theme.palette.grey[600] : theme.palette.grey[400],
+                      ":hover": { color: theme.palette.primary.onSurface },
+                    }}
+                    />
+                }
               </IconButton>
             </Tooltip>
-            <Button variant="text" href="/about" sx={{ borderRadius: "16px" }}>
-              About
-            </Button>
+            <Link href="/about">
+              <Button variant="text" sx={{ borderRadius: "16px" }}>
+                About
+              </Button>
+            </Link>
 
-            <Button variant="text" href="/work" sx={{ borderRadius: "16px" }}>
-              Work
-            </Button>
+            <Link href="/work">
+              <Button variant="text" sx={{ borderRadius: "16px" }}>
+                Work
+              </Button>
+            </Link>
 
-            <Button variant="contained" href="/contact" sx={{ borderRadius: "16px" }}>
-              Contact
-            </Button>
+            <Link href="/contact">
+              <Button variant="contained" sx={{ borderRadius: "16px" }}>
+                Contact
+              </Button>
+            </Link>
           </Stack>
         </Stack>
       </Grid>
