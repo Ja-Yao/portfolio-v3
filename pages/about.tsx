ab0d95015deeb09767369d0ac35058f11ff5f11b
jacob.yao <jacob@blackcape.io>
2023-01-08 15:32:38 -0500

    Add banner image and header

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,13 @@
 import styles from '../styles/Home.module.css';
-import { Box } from "@mui/material";
+import { motion } from 'framer-motion';
+
+import { Box, Typography } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+
+import Image from 'next/image';
+import banner from '../public/banner.png';
 
 
 export default function About() {
@@ -11,18 +16,37 @@ export default function About() {
   return (
     <Box className={styles.container} sx={{ backgroundColor: theme.palette.primary.surface }}>
       <Navbar />
-      <Box component="main" sx={{
-        minHeight: "100vh",
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        width: "min(100% - 2rem, 1500px)",
-        marginInline: "auto",
-        overflow: "hidden",
-      }}>
-        
+      <Box component="main" className={styles.main}>
+        {/* Landing */}
+        <motion.div
+          viewport={{ once: true }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.25 }}
+          style={{
+            marginTop: "7.5vh",
+            display: "grid",
+            gridTemplateColumns: "100%",
+            gridTemplateRows: "1fr",
+            placeItems: "center"
+          }}
+        >
+          <Image quality={90} src={banner} placeholder="blur" alt="banner-image" style={{ borderRadius: "28px", gridColumn: 1, gridRow: 1 }} />
+          <Typography
+            variant="h1"
+            textAlign="center"
+            sx={{
+              gridColumn: 1,
+              gridRow: 1,
+              zIndex: 1,
+              color: theme.palette.primary.contrastText,
+            }}
+          >
+            About Me
+          </Typography>
+        </motion.div>
+
+
       </Box>
       <Footer />
     </Box>
