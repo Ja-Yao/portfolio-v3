2a8281d8e8648a70e1a6f96a826f5e1fe16c844c
jacob.yao <jacob@blackcape.io>
2023-01-08 11:31:06 -0500

    Setup about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,30 @@
+import styles from '../styles/Home.module.css';
+import { Box } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+
 export default function About() {
-    return (
-        <>
-        </>
-    )
+  const theme = useTheme();
+
+  return (
+    <Box className={styles.container} sx={{ backgroundColor: theme.palette.primary.surface }}>
+      <Navbar />
+      <Box component="main" sx={{
+        minHeight: "100vh",
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: "min(100% - 2rem, 1500px)",
+        marginInline: "auto",
+        overflow: "hidden",
+      }}>
+        
+      </Box>
+      <Footer />
+    </Box>
+  )
 };
