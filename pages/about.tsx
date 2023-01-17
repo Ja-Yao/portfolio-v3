04007479ee05c88320704c97da8d4644130b23fc
jacob.yao <jacob@blackcape.io>
2023-01-16 23:03:32 -0500

    Add main content sections

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/Home.module.css';
 import { motion, Variants } from 'framer-motion';
 
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, Divider, Stack, Typography } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -62,24 +62,20 @@ export default function About() {
           </Typography>
         </motion.div>
 
-        {/* About */}
-        <motion.div
-          id="landing-section"
-          variants={works}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          style={{ marginTop: "7.5vh" }}
-        >
-          <Stack direction="row" alignItems="flex-start" justifyContent="space-evenly" >
-            <Stack direction="column" spacing={2} sx={{ width: "25%" }}>
-              <motion.h1
-                style={{ color: theme.palette.primary.contrastText }}
-              >
+        <Box className={styles.grid} sx={{ mt: 4 }}>
+          <Box className={styles.card} sx={{ maxWidth: '50%' }}>
+            <Stack direction="column" spacing={1}>
+              <Typography style={{ color: theme.palette.primary.contrastText }} >
                 My Background
+              </Typography>
+              <motion.h1 style={{ color: theme.palette.primary.contrastText }} >
+                Application <span style={{ color: theme.palette.primary.main }}>Development</span>
               </motion.h1>
             </Stack>
-            <Stack direction="column" spacing={1} sx={{ maxWidth: "40%" }}>
+          </Box>
+
+          <Box className={styles.card} sx={{ maxWidth: '50%' }}>
+            <Stack direction="column" spacing={1}>
               <Typography
                 sx={{
                   color: theme.palette.primary.contrastText,
@@ -97,20 +93,88 @@ export default function About() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Typography>
             </Stack>
-          </Stack>
-        </motion.div>
+          </Box>
 
-        {/* Process */}
-        <motion.div
-          id="landing-section"
-          variants={works}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          style={{ marginTop: "7.5vh" }}
-        >
-          <Stack direction="row" alignItems="flex-start" justifyContent="space-evenly" >
-            <Stack direction="column" spacing={2} sx={{ maxWidth: "40%" }}>
+          <Card elevation={0} sx={{ mt: 14, mb: 16, backgroundColor: theme.palette.primary.surfaceAt1, borderRadius: "28px", padding: 2 }}>
+            <CardHeader
+              title='My Journey'
+              subheader="A brief overview of the experiences I've had"
+              action={
+                <Button variant='outlined' onClick={e => { e.preventDefault(); window.open('https://firebasestorage.googleapis.com/v0/b/portfolio-contact-form-47fb6.appspot.com/o/Jacob-Yao-Resume.pdf?alt=media&token=235d3d8f-4179-4065-b567-a80dd456453e', "_blank") }}>
+                  View my Resume
+                </Button>
+              }
+            />
+            <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', flexWrap: 'wrap' }}>
+              <Box sx={{ mr: 8 }}>
+                <Stack direction='row' spacing={1}>
+                  <Divider orientation='vertical' variant='middle' flexItem sx={{ color: theme.palette.primary.contrastText }} />
+                  <Stack direction='column'>
+                    <Typography variant='h6'>
+                      Swinburne University of Technology
+                    </Typography>
+                    <Typography variant='subtitle1' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      Melbourne, Victoria, Australia
+                    </Typography>
+                    <Typography variant='caption' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      July 2019 – November 2019
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Box>
+              <Box sx={{ mr: 8 }}>
+                <Stack direction='row' spacing={1}>
+                  <Divider orientation='vertical' variant='middle' flexItem sx={{ color: theme.palette.primary.contrastText }} />
+                  <Stack direction='column'>
+                    <Typography variant='h6'>
+                      Northeastern University
+                    </Typography>
+                    <Typography variant='subtitle1' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      Boston, Massachusetts
+                    </Typography>
+                    <Typography variant='caption' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      January 2020 – Present
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Box>
+              <Box sx={{ mr: 8 }}>
+                <Stack direction='row' spacing={1}>
+                  <Divider orientation='vertical' variant='middle' flexItem sx={{ color: theme.palette.primary.contrastText }} />
+                  <Stack direction='column'>
+                    <Typography variant='h6'>
+                      Black Cape
+                    </Typography>
+                    <Typography variant='subtitle1' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      Arlington, Virginia
+                    </Typography>
+                    <Typography variant='caption' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      May 2021 – December 2021
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Box>
+              <Box sx={{ mr: 8 }}>
+                <Stack direction='row' spacing={1}>
+                  <Divider orientation='vertical' variant='middle' flexItem sx={{ color: theme.palette.primary.contrastText }} />
+                  <Stack direction='column'>
+                    <Typography variant='h6'>
+                      Black Cape
+                    </Typography>
+                    <Typography variant='subtitle1' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      Arlington, Virginia
+                    </Typography>
+                    <Typography variant='caption' sx={{ fontStyle: 'italic', color: theme.palette.grey[500] }}>
+                      July 2022 – Present
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Box>
+            </CardContent>
+          </Card>
+
+          <Box className={styles.card} sx={{ maxWidth: '50%' }}>
+            <Stack direction="column" spacing={1} >
               <Typography
                 sx={{ color: theme.palette.primary.contrastText }}
               >
@@ -123,9 +187,12 @@ export default function About() {
                 User <span style={{ color: theme.palette.primary.main }}>Experience</span>
               </motion.h1>
             </Stack>
-            <Stack direction="column" spacing={1} sx={{ maxWidth: "40%" }}>
+          </Box>
+
+          <Box className={styles.card} sx={{ maxWidth: '50%' }}>
+            <Stack direction="column" spacing={1}>
               <Typography
-                sx={{ 
+                sx={{
                   color: theme.palette.primary.contrastText,
                   fontFamily: "PT Sans"
                 }}
@@ -133,7 +200,7 @@ export default function About() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Typography>
               <Typography
-                sx={{ 
+                sx={{
                   color: theme.palette.primary.contrastText,
                   fontFamily: "PT Sans"
                 }}
@@ -141,8 +208,8 @@ export default function About() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Typography>
             </Stack>
-          </Stack>
-        </motion.div>
+          </Box>
+        </Box>
       </Box>
       <Footer />
     </Box>
