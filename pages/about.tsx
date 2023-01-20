41d6e5ab9ce0477e0f9187908770afe5ba451bd8
jacob.yao <jacob@blackcape.io>
2023-01-19 23:54:45 -0500

    Add description for skillset section

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css';
 import { motion, Variants } from 'framer-motion';
 
 import { Box, Button, Card, CardContent, CardHeader, Divider, Stack, Typography } from "@mui/material";
+import Grid2 from '@mui/material/Unstable_Grid2';
 import { useTheme } from '@mui/material/styles';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -64,25 +65,37 @@ export default function About() {
 
         <Box className={styles.grid} sx={{ mt: 4 }}>
           <Box className={styles.card} sx={{ maxWidth: '50%' }}>
-            <Stack direction="column" spacing={1}>
-              <Typography style={{ color: theme.palette.primary.contrastText }} >
-                My Background
-              </Typography>
-              <motion.h1 style={{ color: theme.palette.primary.contrastText }} >
-                Application <span style={{ color: theme.palette.primary.main }}>Development</span>
-              </motion.h1>
+            <Stack direction='column'>
+              <Stack direction="column" spacing={1} >
+                <Typography
+                  sx={{ color: theme.palette.primary.contrastText }}
+                >
+                  My Skillset
+                </Typography>
+                <motion.h1
+                  style={{ color: theme.palette.primary.contrastText }}
+                >
+                  Frontend <span style={{ color: theme.palette.primary.main }}>Development</span> & <br />
+                  User <span style={{ color: theme.palette.primary.main }}>Experience</span>
+                </motion.h1>
+              </Stack>
+              <Stack direction="row" spacing={8} sx={{ marginTop: '33%', justifyContent: 'flex-start' }}>
+                <Image src="/typescript.png" alt="Figma logo" width={64} height={64} />
+                <Image src="/nextjs.png" alt="Next.js logo" width={64} height={64} />
+                <Image src="/figma.png" alt="Figma logo" width={64} height={64} />
+              </Stack>
             </Stack>
           </Box>
 
           <Box className={styles.card} sx={{ maxWidth: '50%' }}>
-            <Stack direction="column" spacing={1}>
+            <Stack direction="column" spacing={2}>
               <Typography
                 sx={{
                   color: theme.palette.primary.contrastText,
                   fontFamily: "PT Sans"
                 }}
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                While I've created Android applications with Java and XML in the past, my skillset is centered around JavaScript, TypeScript, HTML, and CSS. Most of the work using these languages have been in React projects, as well as Remix and Next.js — including this website! Regarding UI libraries, I've had the most experience with Material UI, but I'd like to try out TailwindCSS, NextUI, and Material Web to name a few.
               </Typography>
               <Typography
                 sx={{
@@ -90,7 +103,7 @@ export default function About() {
                   fontFamily: "PT Sans"
                 }}
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I began my deep dive into UI/UX in January 2022, where I first learned about core design principles and how to implement them. Over the summer and fall of that year, I participated in a few of the Google UX Design classes. While in these classes, I learned how to research and identify user needs, create user personas, journey maps, and storyboards, generate low-fidelity prototypes, and conduct usability studies. With the tools I've gained, I've been able to improve & create high-fidelity prototypes in Figma, leading to full design implementation in products I've worked on.
               </Typography>
             </Stack>
           </Box>
@@ -105,7 +118,7 @@ export default function About() {
                 </Button>
               }
             />
-            <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', flexWrap: 'wrap' }}>
+            <CardContent sx={{ display: 'flex', flexWrap: 'wrap' }}>
               <Box sx={{ mr: 8 }}>
                 <Stack direction='row' spacing={1}>
                   <Divider orientation='vertical' variant='middle' flexItem sx={{ color: theme.palette.primary.contrastText }} />
@@ -174,17 +187,18 @@ export default function About() {
           </Card>
 
           <Box className={styles.card} sx={{ maxWidth: '50%' }}>
-            <Stack direction="column" spacing={1} >
-              <Typography
-                sx={{ color: theme.palette.primary.contrastText }}
-              >
-                My Skillset
+            <Stack direction="column" spacing={1}>
+              <Typography style={{ color: theme.palette.primary.contrastText }} >
+                My Interests
               </Typography>
-              <motion.h1
-                style={{ color: theme.palette.primary.contrastText }}
-              >
-                Frontend <span style={{ color: theme.palette.primary.main }}>Development</span> & <br />
-                User <span style={{ color: theme.palette.primary.main }}>Experience</span>
+              <motion.h1 style={{ color: theme.palette.primary.contrastText }} >
+                Capturing <span style={{ color: theme.palette.primary.main }}>Memories, </span> <span style={{ color: theme.palette.primary.surface }}>&&</span>
+              </motion.h1>
+              <motion.h1 style={{ color: theme.palette.primary.contrastText }} >
+                Making <span style={{ color: theme.palette.primary.main }}>Music</span> &
+              </motion.h1>
+              <motion.h1 style={{ color: theme.palette.primary.contrastText }} >
+                More...
               </motion.h1>
             </Stack>
           </Box>
